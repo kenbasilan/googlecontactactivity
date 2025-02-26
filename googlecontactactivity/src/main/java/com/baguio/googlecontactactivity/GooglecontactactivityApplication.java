a9724544b28bc15ec7c70838a063fa1b073aef8d
LAB14_USER ON WS22 <L14X08W22@citulab.net>
2025-02-26 08:35:10 +0800

    added a file

---
+++ googlecontactactivity/src/main/java/com/baguio/googlecontactactivity/GooglecontactactivityApplication.java
@@ -0,0 +1,13 @@
+package com.baguio.googlecontactactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglecontactactivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglecontactactivityApplication.class, args);
+	}
+
+}
---
+++ googlecontactactivity/src/test/java/com/baguio/googlecontactactivity/GooglecontactactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baguio.googlecontactactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglecontactactivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
